dd2294a1d5c1309bdea101108a61370b7f8a33c0
thanhbinhs <thanhbinhsmart@gmail.com>
2024-09-07 11:23:03 +0700

    update color

--- frontend/src/themes/darkTheme.jsx
+++ frontend/src/themes/darkTheme.jsx
@@ -1,6 +1,8 @@
 export const darkTheme = {
-    body: '#000',
-    text: '#FFF',
+    navbar: '#FFEDD5',
+    button1:'#FDBA74',
+    black: '#000',
+    while : '#FFF',
     toggleBorder: '#6B8096',
     background: '#999',
   };
