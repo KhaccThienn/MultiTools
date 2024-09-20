fee2806987f8a05f65da7bade4c08f67318b8f82
nguyentrunghieu2411 <124479937+nguyentrunghieu2411@users.noreply.github.com>
2024-09-20 15:29:31 +0700

    thêm footer editor UI

--- frontend/src/components/FooterEditor.jsx
+++ frontend/src/components/FooterEditor.jsx
@@ -1,7 +1,38 @@
-export default function FooterEditor() {
-    return (
-        <div>
-            Footer
-        </div>
-    );
+import React from 'react';
+import './App.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+function App() {
+  return (
+    <div className="toolbar">
+      <div className='ajust-size-section'>
+        <button className="icon-button">
+          <i className="fa fa-search-minus"></i>
+        </button>
+        <span className="zoom-text">52%</span>
+        <button className="icon-button">
+         <i className="fa fa-search-plus"></i>
+        </button>
+      </div>
+      <div className='undo-redo-section'>
+        <button className="toolbar-button">
+          <span className="zoom-text">HOÀN TÁC</span>
+          <i className="fas fa-undo icon-custom"></i>
+        </button>
+        <button className="toolbar-button">
+          <i className="fas fa-redo icon-custom"></i>
+          <span className="zoom-text">HOÀN LẠI</span>
+        </button>
+      </div>
+      <button className="toolbar-button close">
+       <span>Đóng</span>
+      </button>  
+      <button className="toolbar-button save-button">
+        <i class="fa-solid fa-download"></i>
+       <span>Lưu</span> 
+      </button>
+    </div>
+  );
 }
+
+export default App;
